1cdaf6ca22b50589a8ddfc85cc119e833c66ccb4
kranthivr <kranthi.neo1@gmail.com>
2024-04-05 09:23:46 +0530

    config and landing page changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,6 @@ function Hero() {
         id="home"
         mt="60px"
         direction={{ xs: "column", md: "row-reverse" }}
-        spacing={4}
         sx={{
           backgroundImage: `url(${hbg})`,
           backgroundSize: "cover",
@@ -22,43 +21,52 @@ function Hero() {
           zIndex: 5,
         }}
       >
-        <Box
-          component="img"
-          src={profilePic}
-          sx={{
-            width: { xs: "10rem", md: "15rem" },
-            height: { xs: "10rem", md: "15rem" },
-            borderRadius: "50%",
-            zIndex: 5,
-          }}
-        />
-
-        <Typography
+        <Stack
+          direction="row"
+          alignItems="center"
           fontWeight="fontWeightBold"
+          gap={2}
           sx={{
             fontSize: { xs: "2rem", md: "2rem" },
             fontFamily: "Nunito",
             color: "#424242",
+            backgroundColor: "white",
+            padding: "1rem 1rem",
+            height: { xs: "10rem", md: "15rem" },
             zIndex: 5,
+            borderRadius: "1rem",
           }}
         >
-          <svg
-            version="1.0"
-            xmlns="http://www.w3.org/2000/svg"
-            width="100"
-            height="100"
-          >
-            <path
-              fill="#020202"
-              d="m66.666 36.67-4.713 4.713 8.619 8.62-8.622 8.62 4.713 4.713L80 50.003zM33.333 63.336l4.714-4.713-8.62-8.62 8.623-8.62-4.714-4.713L20 50.003z"
-            />
-            <path
-              fill="#00B8D4"
-              d="m38.973 78.275 15.612-58.27 6.44 1.725L45.412 80z"
-            />
-          </svg>
-          Hi, I'm Kranthi <br /> Full Stack Developer
-        </Typography>
+          <Box>
+            <svg
+              version="1.0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="100"
+              height="100"
+            >
+              <path
+                fill="#020202"
+                d="m66.666 36.67-4.713 4.713 8.619 8.62-8.622 8.62 4.713 4.713L80 50.003zM33.333 63.336l4.714-4.713-8.62-8.62 8.623-8.62-4.714-4.713L20 50.003z"
+              />
+              <path
+                fill="#00B8D4"
+                d="m38.973 78.275 15.612-58.27 6.44 1.725L45.412 80z"
+              />
+            </svg>
+            Hi, I'm Kranthi <br /> Full Stack Developer
+          </Box>
+          <Box
+            component="img"
+            src={profilePic}
+            sx={{
+              width: { xs: "10rem", md: "15rem" },
+              height: { xs: "10rem", md: "15rem" },
+              borderRadius: "50%",
+              border: "solid 2px black",
+              zIndex: 5,
+            }}
+          />
+        </Stack>
       </Stack>
     </>
   );
