7a0f2369c5d1e65cdacf5f5f21c8bee4b33c1da7
kranthivr <kranthi.neo1@gmail.com>
2024-06-17 01:02:59 +0530

    xp duration with dayjs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,11 @@
 import { Box, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { xps } from "../data";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
 
 function About() {
+  dayjs.extend(duration);
   const [xpText, setXpText] = useState(getXpTimeDuration());
 
   const twoPadStart = (num) => {
@@ -26,17 +29,42 @@ function About() {
       return accumulator + (endDate - startDate);
     }, 0);
 
-    const years = Math.floor(xpTime / (1000 * 60 * 60 * 24 * 365.25));
-    let months = Math.ceil(xpTime / (1000 * 60 * 60 * 24 * 30.4375)) % 12;
-    //let days = Math.floor(Math.floor(xpTime / (1000 * 60 * 60 * 24)) % 30.4375);
-    //let hours = Math.floor(Math.floor(xpTime / (1000 * 60 * 60)) % 24);
-    //let minutes = Math.floor(Math.floor(xpTime / (1000 * 60)) % 60);
-    //let seconds = Math.floor(Math.floor(xpTime / 1000) % 60);
+    // const years = Math.round(xpTime / (1000 * 60 * 60 * 24 * 365.25));
+    // let months = Math.ceil(xpTime / (1000 * 60 * 60 * 24 * 30.4375)) % 12;
+    // //let days = Math.floor(Math.floor(xpTime / (1000 * 60 * 60 * 24)) % 30.4375);
+    // //let hours = Math.floor(Math.floor(xpTime / (1000 * 60 * 60)) % 24);
+    // //let minutes = Math.floor(Math.floor(xpTime / (1000 * 60)) % 60);
+    // //let seconds = Math.floor(Math.floor(xpTime / 1000) % 60);
 
-    let monthsText = `${months.toString()} ${months > 1 ? "Months" : "Month"}`;
-    //let daysText = `${days.toString()} ${days > 1 ? "Days" : "Day"}`;
+    // let monthsText = `${months.toString()} ${months > 1 ? "Months" : "Month"}`;
+    // //let daysText = `${days.toString()} ${days > 1 ? "Days" : "Day"}`;
 
-    return `${years} Years, ${monthsText}`;
+    // return `${years} Years, ${monthsText}`;
+
+    const years = dayjs.duration(xpTime).years();
+    const yearText = `${
+      years > 0 ? years + ` ${years > 1 ? "years" : "year"}` : ""
+    }`;
+    const months = dayjs.duration(xpTime).months();
+    const monthText = `${
+      months > 0 ? months + ` ${months > 1 ? "months" : "month"}` : ""
+    }`;
+    const days = dayjs.duration(xpTime).days();
+    const dayText = `${days > 0 ? days + ` ${days > 1 ? "days" : "day"}` : ""}`;
+    const hours = dayjs.duration(xpTime).hours();
+    const hourText = `${
+      hours > 0 ? hours + ` ${hours > 1 ? "hours" : "hour"}` : ""
+    }`;
+    const minutes = dayjs.duration(xpTime).minutes();
+    const minuteText = `${
+      minutes > 0 ? minutes + ` ${minutes > 1 ? "minutes" : "minute"}` : ""
+    }`;
+    const seconds = dayjs.duration(xpTime).seconds();
+    const secondText = `${
+      seconds > 0 ? seconds + ` ${seconds > 1 ? "seconds" : "second"}` : ""
+    }`;
+    const text = [yearText, monthText].filter(Boolean).join(", ");
+    return text;
   }
 
   return (
